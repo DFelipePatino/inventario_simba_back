2652cbf41616e1069c665ceb9df8b53ae24cb5b7
Daniel Patino <dafepa10c29@gmail.com>
2025-02-23 14:06:23 -0500

    allowed access CORS

--- inventario_simba/settings.py
+++ inventario_simba/settings.py
@@ -86,6 +86,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "https://inventario-simba-front.onrender.com",
+    "https://danielstore-front.onrender.com"
 ]
 
 ROOT_URLCONF = 'inventario_simba.urls'
